79c4fc546c0a5747536b7c2906a121542934d231
mitchiet <mitchiet888@gmail.com>
2025-06-07 14:57:55 -0500

    Added tailwind catalyst components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Navbar } from './components/tailwind-catalyst/navbar'
+import { Sidebar } from './components/tailwind-catalyst/sidebar'
+import { SidebarLayout } from './components/tailwind-catalyst/sidebar-layout'
+
 function App() {
-   
-return (
-  <>
-    Hello World
-  </>
-);
+  return (
+    <SidebarLayout
+      navbar={
+        <Navbar>
+        </Navbar>
+      }
+      sidebar={
+        <Sidebar>
+        </Sidebar>
+      }
+    >
+    </SidebarLayout>
+  )
 }
 
 export default App
