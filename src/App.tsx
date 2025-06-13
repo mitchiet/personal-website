058f12a1ded69e0c740a239a4e7443195639f841
mitchiet <mitchiet888@gmail.com>
2025-06-13 11:05:17 -0500

    Added a mobile sidebar.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Switch } from './components/tailwind-catalyst/switch'
 import * as Headless from '@headlessui/react'
 
 import { Bars3Icon } from '@heroicons/react/24/solid'
+import { ArrowLeftIcon } from '@heroicons/react/24/solid'
 
 import SkillsSection from "./components/sections/SkillsSection"
 import ExperienceSection from './components/sections/ExperiencesSection'
@@ -54,12 +55,35 @@ function App() {
     }
   }
 
+  let [showSidebar, setShowSidebar] = useState(false)
+
   return (
     <div className="p-6 lg:p-10">
 
+    {/* Mobile Sidebar */}
+    <Headless.Dialog open={showSidebar} onClose={() => setShowSidebar(false)} className="lg:hidden">
+      <Headless.DialogBackdrop
+        transition
+        className="fixed inset-0 bg-black/30 transition data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+      />
+      <Headless.DialogPanel
+        transition
+        className="fixed z-100 inset-y-0 w-full max-w-80 p-2 transition duration-300 ease-in-out data-closed:-translate-x-full"
+      >
+        <div className="flex h-full flex-col rounded-lg bg-white shadow-xs ring-1 ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10">
+          <div className="-mb-3 px-4 pt-3">
+            <Headless.CloseButton as={NavbarItem} aria-label="Close navigation" className="float-end">
+              <ArrowLeftIcon />
+            </Headless.CloseButton>
+          </div>
+          {/* {children} */}
+        </div>
+      </Headless.DialogPanel>
+    </Headless.Dialog>
+
       {/* Navbar section */}
       <Navbar id="main-nav" className="fixed top-0 left-0 right-0 z-50 opacity-100 border-double border-b-4">
-        <Button plain className="lg:hidden ml-3">
+        <Button plain className="lg:hidden ml-3" onClick={() => setShowSidebar(true)}>
           <Bars3Icon />
         </Button>
         <NavbarSpacer />
