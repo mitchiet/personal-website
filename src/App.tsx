3343988e25120dd996b0459a432595a38d709d9c
mitchiet <mitchiet888@gmail.com>
2025-06-13 00:16:13 -0500

    Ditching the sidebar layout, will create my own navbar + sidebar layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useLayoutEffect, useState } from 'react'
 
+import { Button } from './components/tailwind-catalyst/button'
 import { Label } from './components/tailwind-catalyst/fieldset'
-import { Navbar } from './components/tailwind-catalyst/navbar'
-import { Sidebar } from './components/tailwind-catalyst/sidebar'
-import { SidebarLayout } from './components/tailwind-catalyst/sidebar-layout'
+import { Navbar, NavbarItem, NavbarSpacer } from './components/tailwind-catalyst/navbar'
 import { Switch } from './components/tailwind-catalyst/switch'
 import * as Headless from '@headlessui/react'
+
+import { Bars3Icon } from '@heroicons/react/24/solid'
 
 import SkillsSection from "./components/sections/SkillsSection"
 import ExperienceSection from './components/sections/ExperiencesSection'
@@ -54,23 +55,29 @@ function App() {
   }
 
   return (
-    <SidebarLayout
-      navbar={
-        <Navbar />
-      }
-      sidebar={
-        <Sidebar />
-      }
-    >
-      <Headless.Field className="flex float-end items-center gap-2">
-        <Label className="dark:opacity-50">Light</Label>
-        <Switch checked={darkMode} onChange={toggleDarkMode} />
-        <Label className="opacity-50 dark:opacity-100">Dark</Label>
-      </Headless.Field>
-      <ExperienceSection />
-      <br/><br/><br/><br/>
-      <SkillsSection />
-    </SidebarLayout>
+    <div className="p-6 lg:p-10">
+
+      {/* Navbar section */}
+      <Navbar id="main-nav" className="fixed top-0 left-0 right-0 z-50 opacity-100 border-double border-b-4">
+        <Button plain className="lg:hidden ml-3">
+          <Bars3Icon />
+        </Button>
+        <NavbarSpacer />
+        <Headless.Field className="flex float-end items-center gap-2 mr-3">
+          <Label className="dark:opacity-50">Light</Label>
+          <Switch checked={darkMode} onChange={toggleDarkMode} />
+          <Label className="opacity-50 dark:opacity-100">Dark</Label>
+        </Headless.Field>
+      </Navbar>
+
+      {/* Main section */}
+      <div className="mt-20">
+        <ExperienceSection />
+        <br/><br/><br/><br/>
+        <SkillsSection />
+      </div>
+
+    </div>
   )
 }
 
