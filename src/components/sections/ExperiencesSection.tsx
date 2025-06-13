7e424bde284564afaa018527913f1b160790c46b
mitchiet <mitchiet888@gmail.com>
2025-06-12 22:44:29 -0500

    Made experiences page use a swiper grid.

--- src/components/sections/ExperiencesSection.tsx
+++ src/components/sections/ExperiencesSection.tsx
@@ -1,8 +1,9 @@
 import { Heading } from '../tailwind-catalyst/heading'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Grid, Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import "swiper/css/grid";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
@@ -18,36 +19,45 @@ function ExperiencesSection() {
         <Heading>Experiences</Heading>
       </div>
       <br/>
-      <Swiper navigation pagination modules={[Navigation, Pagination]}>
+      <Swiper slidesPerView={1}
+          grid={{
+            rows: 2,
+            fill: "row"
+          }}
+          navigation
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Grid, Navigation, Pagination]}>
         <SwiperSlide>
-          <div className="mb-10">
-            <div className="flex items-center justify-center">
-              <img className="dark:hidden w-7/10" src={l3harrisLogo}></img>
-              <img className="hidden dark:block w-7/10" src={l3harrisDarkLogo}></img>
-            </div>
-            <div className="mx-15">
-              <div className="flow-root">
-                <h1 className="float-left underline">L3Harris</h1>
-                <h1 className="float-right">2020 - Present</h1>
-              </div>
-              <h1 className="text-left italic">Greenville, Texas</h1>
-              <h1 className="text-left italic">Sr. Associate Software Engineer</h1>
-            </div>
+          <div className="flex items-center justify-center">
+            <img className="dark:hidden w-7/10" src={l3harrisLogo}></img>
+            <img className="hidden dark:block w-7/10" src={l3harrisDarkLogo}></img>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="my-10">
-            <div className="flex items-center justify-center">
-              <img className="dark:bg-gray-400 dark:border-2 w-6/10" src={minigripLogo}></img>
+          <div className="flex items-center justify-center">
+            <img className="dark:bg-gray-400 dark:border-2 w-3/5" src={minigripLogo}></img>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mx-15">
+            <div className="flow-root">
+              <h1 className="float-left underline">L3Harris</h1>
+              <h1 className="float-right">2020 - Present</h1>
             </div>
-            <div className="mx-15 mt-10">
-              <div className="flow-root">
-                <h1 className="float-left underline">Minigrip</h1>
-                <h1 className="float-right">2019</h1>
-              </div>
-              <h1 className="text-left italic">Seguin, Texas</h1>
-              <h1 className="text-left italic">Technical Intern</h1>
+            <h1 className="text-left italic">Greenville, Texas</h1>
+            <h1 className="text-left italic">Sr. Associate Software Engineer</h1>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mx-15">
+            <div className="flow-root">
+              <h1 className="float-left underline">Minigrip</h1>
+              <h1 className="float-right">2019</h1>
             </div>
+            <h1 className="text-left italic">Seguin, Texas</h1>
+            <h1 className="text-left italic">Technical Intern</h1>
           </div>
         </SwiperSlide>
       </Swiper>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,8 @@
 
 // For swiper
 declare module 'swiper/css' {}
+declare module 'swiper/css/grid' {}
+declare module 'swiper/css/navigation' {}
 declare module 'swiper/css/pagination' {}
 declare module 'swiper/css/autoplay' {}
 declare module 'swiper/css/effect-fade' {}
-declare module 'swiper/css/navigation' {}
