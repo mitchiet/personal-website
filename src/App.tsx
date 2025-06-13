e219128933f31caf999021434f42b1a468b3ea29
mitchiet <mitchiet888@gmail.com>
2025-06-13 12:12:06 -0500

    Added a sidebar.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useLayoutEffect, useState } from 'react'
 import { Button } from './components/tailwind-catalyst/button'
 import { Label } from './components/tailwind-catalyst/fieldset'
 import { Navbar, NavbarItem, NavbarSpacer } from './components/tailwind-catalyst/navbar'
+import { Sidebar } from './components/tailwind-catalyst/sidebar'
 import { Switch } from './components/tailwind-catalyst/switch'
 import * as Headless from '@headlessui/react'
 
@@ -58,47 +59,62 @@ function App() {
   let [showSidebar, setShowSidebar] = useState(false)
 
   return (
-    <div className="p-6 lg:p-10">
-
-    {/* Mobile Sidebar */}
-    <Headless.Dialog open={showSidebar} onClose={() => setShowSidebar(false)} className="lg:hidden">
-      <Headless.DialogBackdrop
-        transition
-        className="fixed inset-0 bg-black/30 transition data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
-      />
-      <Headless.DialogPanel
-        transition
-        className="fixed z-100 inset-y-0 w-full max-w-80 p-2 transition duration-300 ease-in-out data-closed:-translate-x-full"
-      >
-        <div className="flex h-full flex-col rounded-lg bg-white shadow-xs ring-1 ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10">
-          <div className="-mb-3 px-4 pt-3">
-            <Headless.CloseButton as={NavbarItem} aria-label="Close navigation" className="float-end">
-              <ArrowLeftIcon />
-            </Headless.CloseButton>
-          </div>
-          {/* {children} */}
-        </div>
-      </Headless.DialogPanel>
-    </Headless.Dialog>
+    <div>
 
       {/* Navbar section */}
-      <Navbar id="main-nav" className="fixed top-0 left-0 right-0 z-50 opacity-100 border-double border-b-4">
+      <Navbar id="main-nav" className="fixed top-0 left-0 right-0 z-100 opacity-100 border-double border-b-4">
         <Button plain className="lg:hidden ml-3" onClick={() => setShowSidebar(true)}>
           <Bars3Icon />
         </Button>
         <NavbarSpacer />
-        <Headless.Field className="flex float-end items-center gap-2 mr-3">
+        <Headless.Field className="flex float-end items-center gap-2 mr-3 p-3">
           <Label className="dark:opacity-50">Light</Label>
           <Switch checked={darkMode} onChange={toggleDarkMode} />
           <Label className="opacity-50 dark:opacity-100">Dark</Label>
         </Headless.Field>
       </Navbar>
 
-      {/* Main section */}
-      <div className="mt-20">
-        <ExperienceSection />
-        <br/><br/><br/><br/>
-        <SkillsSection />
+      <div className="p-6 lg:p-10 flex max-lg:flex-col relative isolate min-h-svh w-full">
+
+        {/* Desktop Sidebar */}
+        <div className="fixed mt-14 inset-y-0 left-0 w-64 max-lg:hidden z-50 border-r-2 border-zinc-950/10 dark:border-white/10 lg:bg-zinc-100 dark:lg:bg-zinc-950">
+          <Sidebar>
+            {/* {children} */}
+          </Sidebar>
+        </div>
+        
+        {/* Mobile Sidebar */}
+        <Headless.Dialog open={showSidebar} onClose={() => setShowSidebar(false)} className="lg:hidden">
+          <Headless.DialogBackdrop
+            transition
+            className="fixed inset-0 bg-black/30 transition data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+          />
+          <Headless.DialogPanel
+            transition
+            className="fixed z-100 inset-y-0 w-full max-w-80 p-2 transition duration-300 ease-in-out data-closed:-translate-x-full"
+          >
+            <div className="flex h-full flex-col rounded-lg bg-white shadow-xs ring-1 ring-zinc-950/5 dark:bg-zinc-900 dark:ring-white/10">
+              <div className="-mb-3 px-4 pt-3">
+                <Headless.CloseButton as={NavbarItem} aria-label="Close navigation" className="float-end">
+                  <ArrowLeftIcon />
+                </Headless.CloseButton>
+              </div>
+              {/* {children} */}
+            </div>
+          </Headless.DialogPanel>
+        </Headless.Dialog>
+
+        {/* Main section */}
+        <main className="flex flex-1 flex-col pb-2 lg:min-w-0 mt-10 lg:mt-0 lg:pt-2 lg:pr-2 lg:pl-64">
+          <div className="grow p-6 lg:p-10">
+            <div className="mx-auto max-w-6xl">
+              <ExperienceSection />
+              <br/><br/><br/><br/>
+              <SkillsSection />
+            </div>
+          </div>
+        </main>
+
       </div>
 
     </div>
--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -7,7 +7,7 @@ function SkillsSection() {
 
   return (
     <div>
-      <div className="flex w-full items-end justify-between gap-4 border-b border-zinc-950/10 pb-6 dark:border-white/10">
+      <div className="flex w-full items-end justify-between gap-4 border-b-2 border-zinc-950/10 pb-4 dark:border-white/10">
         <Heading>Skills</Heading>
       </div>
       <br/>
