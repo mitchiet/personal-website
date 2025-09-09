614534a14b2559b32d40c6cb9c65a24d6d9871ea
mitchiet <mitchiet888@gmail.com>
2025-09-08 20:56:20 -0500

    Giving modals proper background color to prevent fireglass issues.

--- src/components/sections/TechnologiesSection.tsx
+++ src/components/sections/TechnologiesSection.tsx
@@ -325,15 +325,15 @@ function TechnologiesSection({ ref }: TechnologiesSectionProps) {
 
       {ReactDOM.createPortal(
         <div>
-          <AltiumDialog className="absolute flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
+          <AltiumDialog className="absolute modal flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
               isOpen={isAltiumDialogOpen} isOpenSetter={setIsAltiumDialogOpen} isESP32DialogOpenSetter={setIsESP32DialogOpen} />
-          <ESP32Dialog className="absolute flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
+          <ESP32Dialog className="absolute modal flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
               isOpen={isESP32DialogOpen} isOpenSetter={setIsESP32DialogOpen} />
-          <JavaDialog className="absolute flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
+          <JavaDialog className="absolute modal flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
               isOpen={isJavaDialogOpen} isOpenSetter={setIsJavaDialogOpen} />
-          <LaTeXDialog className="absolute flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
+          <LaTeXDialog className="absolute modal flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
               isOpen={isLaTeXDialogOpen} isOpenSetter={setIsLaTeXDialogOpen} />
-          <TailwindCSSDialog className="absolute flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
+          <TailwindCSSDialog className="absolute modal flex flex-col rounded-3xl inset-x-0 mx-auto top-26 max-h-[80vh] min-h-50 max-w-5/6 sm:max-w-lg"
               isOpen={isTailwindCSSDialogOpen} isOpenSetter={setIsTailwindCSSDialogOpen} />
         </div>,
         document.body
