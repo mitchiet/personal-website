fff3dd49069b5eeaa1ca47b957b8e9b9e0a97e8e
mitchiet <mitchiet888@gmail.com>
2025-09-14 11:07:06 -0500

    Making sure the spinner works in fireglass.

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ const LoadingSpinner = () => {
 
   return (
     <div className="flex justify-center items-center intro-swiper">
-      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 spinner-blue"></div>
     </div>
   );
 };
