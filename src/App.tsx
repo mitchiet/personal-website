2c2bc08ba3c36b564a439538492702a128ae0677
mitchiet <mitchiet888@gmail.com>
2025-10-07 18:05:44 -0500

    Angie recommended I switch over to professional headshots.

--- src/App.tsx
+++ src/App.tsx
@@ -212,7 +212,7 @@ function App() {
         <main className="flex flex-1 flex-col isolate lg:min-w-0 lg:pl-64 py-3">
           <div className="scrollable-main px-6 lg:px-10 pt-4 overflow-y-scroll overflow-x-clip scrollbar scroll-pt-4">
             <div className="mx-auto max-w-7xl">
-              <IntroductionSection ref={introductionSectionRef} />
+              <IntroductionSection ref={introductionSectionRef} darkMode={darkMode} />
               <br/><br/><br/><br/>
               <EmploymentSection ref={employmentSectionRef} />
               <br/><br/><br/><br/>
--- src/components/sections/IntroductionSection.tsx
+++ src/components/sections/IntroductionSection.tsx
@@ -1,60 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
-
 import { Heading } from '../tailwind-catalyst/heading'
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCoverflow, FreeMode, Navigation, Pagination } from 'swiper/modules';
-import SwiperCore from 'swiper';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import ImageWithSpinner from '../ImageWithSpinner';
 
-import LoadingSpinner from '../LoadingSpinner';
-
-import pic1 from '../../assets/images/lincoln-memorial.jpg';
-import pic2 from '../../assets/images/hiking.jpg';
-import pic3 from '../../assets/images/suarez.jpg';
-import pic4 from '../../assets/images/volunteer-football.jpg';
-import pic5 from '../../assets/images/concert.jpg';
-
-const images = [pic1, pic2, pic3, pic4, pic5];
+import pic1 from '../../assets/images/headshot.jpg';
+import pic2 from '../../assets/images/headshot-glasses.jpg';
 
 interface IntroductionSectionProps {
     ref?: React.Ref<HTMLDivElement>;
+    darkMode?: boolean;
 }
 
-function IntroductionSection({ ref }: IntroductionSectionProps) {
-  const [allLoaded, setAllLoaded] = useState(false);
-  const swiperRef = useRef<SwiperCore>(null);
-
-  const preloadAndDecode = (src: string): Promise<void> => 
-    new Promise((resolve) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = async () => {
-        try {
-          if (img.decode) {
-            await img.decode();
-          }
-        } catch {
-          // Ignore decoding errors
-        } finally {
-          resolve();
-        }
-      };
-      img.onerror = () => resolve();
-    });
-
-  useEffect(() => {
-    const loadImages = async () => {
-      await Promise.all(images.map(preloadAndDecode));
-      if (swiperRef.current) swiperRef.current.update();
-      setAllLoaded(true);
-    };
-    loadImages();
-  }, []);
+function IntroductionSection({ ref, darkMode }: IntroductionSectionProps) {
 
   return (
     <div ref={ref}>
@@ -68,41 +24,12 @@ function IntroductionSection({ ref }: IntroductionSectionProps) {
         Take a look around!
       </p>
       <br/><br/>
-      <div>
-        {!allLoaded && (<LoadingSpinner />)}
-        {allLoaded && (
-        <Swiper
-            onSwiper={(swiper) => (swiperRef.current = swiper)}
-            className="relative intro-swiper will-change-transform [transform:translateZ(0)]"
-            virtual={false}
-            watchSlidesProgress
-            slidesPerView='auto'
-            effect={'coverflow'}
-            coverflowEffect={{
-              rotate: 50,
-              stretch: 0,
-              depth: 100,
-              modifier: 1,
-              slideShadows: false
-            }}
-            spaceBetween={30}
-            centeredSlides
-            freeMode
-            navigation
-            pagination={{
-              clickable: true,
-            }}
-            modules={[EffectCoverflow, FreeMode, Navigation, Pagination]}>
-          {images.map((src, index) => (
-            <SwiperSlide key={index} className="!w-auto overflow-visible will-change-transform [transform:translateZ(0)]">
-              <img className="rounded-xl border-2 intro-swiper-slide w-auto object-cover block loaded [backface-visibility:hidden] [transform-style:preserve-3d] will-change-transform [transform:translateZ(0)]"
-                src={src}
-                loading="eager"
-                alt={`Slide ${index}`}
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>)}
+      <div className="w-[60%] mx-auto">
+        <ImageWithSpinner 
+          src={darkMode? pic1 : pic2}
+          alt="Senior Design Control Unit"
+          className="rounded-xl border-2"
+        />
       </div>
     </div>
   )
