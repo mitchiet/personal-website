9e523df9851d26d7c53fe6c3ca80359d7a0d4917
mitchiet <mitchiet888@gmail.com>
2025-10-07 18:30:25 -0500

    Removed unused swiper modules.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,13 +3,9 @@
 
 // For swiper
 declare module 'swiper/css' {}
-declare module 'swiper/css/effect-coverflow' {}
-declare module 'swiper/css/free-mode' {}
 declare module 'swiper/css/grid' {}
 declare module 'swiper/css/navigation' {}
 declare module 'swiper/css/pagination' {}
-declare module 'swiper/css/autoplay' {}
-declare module 'swiper/css/effect-fade' {}
 
 declare module '*.svg' {
   import React = require('react');
